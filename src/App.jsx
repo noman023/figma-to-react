3950b86772e8ef08495c34c0d869b291db6af620
Noman Mahmud <molla.noman23@gmail.com>
2023-01-22 17:32:35 +0600

    feat: add components

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import styles from "./styles";
+
+import {
+  Navbar,
+  Hero,
+  Business,
+  FeedBack,
+  Footer,
+  Billing,
+  Button,
+  CTA,
+  CardDeal,
+  Clients,
+  GetStarted,
+  Stats,
+  Testimonials,
+} from "./components";
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Billing />
+          <Business />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
