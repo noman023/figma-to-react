b94c0000bb77476d2c2d2bb6c1d162d4170f5e4e
Noman Mahmud <molla.noman23@gmail.com>
2023-01-23 23:54:21 +0600

    feat: add testimonials

---
+++ README.md
@@ -0,0 +1,5 @@
+![app](public/hoobank.png)
+
+from figma design into a react project.
+
+Figma Design: https://www.figma.com/file/bUGIPys15E78w9bs1l4tgS/HooBank?node-id=310%3A485&t=50pU4ToIM7ZnZuGt-0
--- src/App.jsx
+++ src/App.jsx
@@ -34,8 +34,8 @@ const App = () => {
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Stats />
-          <Billing />
           <Business />
+          <Billing />
           <CardDeal />
           <Testimonials />
           <Clients />
